4ad04a323ee18a1f2ac6f367d84e3be5eb8d2897
Ckituzer <83168446+RawLunaHub@users.noreply.github.com>
2023-07-26 22:43:48 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ if not game:IsLoaded() then
 end
 local plr = game:GetService("Players").LocalPlayer
 local RunService = game:GetService("RunService")
+
 if plr.Name == getgenv().Main then
 else
     --// Main
@@ -12,5 +13,50 @@ else
     end
     if getgenv().BoostCPU == true then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RawLunaHub/AltControl/main/boostcpu.lua")()
+        --// Level 1
+        for _,v in pairs(workspace:GetDescendants()) do
+        if v.ClassName == "Part"
+        or v.ClassName == "SpawnLocation"
+        or v.ClassName == "WedgePart"
+        or v.ClassName == "Terrain"
+        or v.ClassName == "MeshPart" then
+        v.Material = "Plastic"
+        end
+        end
+        --// Level 2
+        for _,v in pairs(workspace:GetDescendants()) do
+        if v.ClassName == "Part"
+        or v.ClassName == "SpawnLocation"
+        or v.ClassName == "WedgePart"
+        or v.ClassName == "Terrain"
+        or v.ClassName == "MeshPart" then
+        v.Material = "Plastic"
+        end
+        end
+         
+        for _,v in pairs(workspace:GetDescendants()) do
+        if v.ClassName == "Decal"
+        or v.ClassName == "Texture" then
+        v:Destroy()
+        end
+        end
+        --// Level 3 
+        for _,v in pairs(workspace:GetDescendants()) do
+        if v.ClassName == "Part"
+        or v.ClassName == "SpawnLocation"
+        or v.ClassName == "WedgePart"
+        or v.ClassName == "Terrain"
+        or v.ClassName == "MeshPart" then
+        v.BrickColor = BrickColor.new(155, 155, 155)
+        v.Material = "Plastic"
+        end
+        end
+         
+        for _,v in pairs(workspace:GetDescendants()) do
+        if v.ClassName == "Decal"
+        or v.ClassName == "Texture" then
+        v:Destroy()
+        end
+        end
     end
 end
